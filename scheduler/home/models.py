428da66b808da365cd100d8c884ae9a1d244cf4e
saranshshk <saranshsh7658@gmail.com>
2025-01-03 15:09:18 +0530

    Initial commit

---
+++ scheduler/home/models.py
@@ -0,0 +1,10 @@
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# # Create your models here.
+# class customuser(AbstractUser):
+#     email=models.EmailField(unique=True)
+#     username=models.CharField(unique=True)
+#     fullname=models.CharField(unique=True)
+
+# def __str__(self):
+#     return self.username
